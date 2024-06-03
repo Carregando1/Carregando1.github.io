9ac1bfb3056fa92c49b68e766d39389b7571d806
Carregando1 <darrenys2015@gmail.com>
2024-06-03 00:20:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,14 @@ function widthcheck() {
 	if (window.innerWidth >= 820) {
 		document.getElementById('totaldiv').innerHTML="<div class='mainbar' id='mainbar1'><button onclick='pg(1)' class='topbar'>Prestige Layers</button></div><div class='mainbar' id='mainbar2'><button onclick='pg(2)' class='topbar'>Locked</button></div><div class='mainbar' id='mainbar3'><button onclick='pg(3)' class='topbar'>Achievements</button></div><div class='mainbar' id='mainbar4'><button onclick='pg(4)' class='topbar'>Statistics</button></div><div id='maincontent'></div>";
 		clearInterval(screencheck);
-		reinit();
+		//reinit();
 	} else {
 		document.getElementById("totaldiv").innerHTML="<p>To play Idle Prestiges, you need a window of width 820px or wider. Resize your window, swap orientation, or swap devices to continue.</p><p>Current width: "+window.innerWidth+"px";
 	}
 }
 function init() {
 	document.getElementById('maincontent').innerHTML="<div class='prestiges'><p class='prestigeinfo'>Score: 10</p><p class='prestigemult'>Multiplier: x1</p><button class='prestigeac' onclick=pres(0)><p>Prestige to Layer 1</p><p>Requires: 10 Score</p></button></div>";
+	document.getElementsByClassName('prestiges')[0].style.top = 60px;
 	screencheck = setInterval(widthcheck, 20);
 	gameintv = setInterval(update, 1000);
 }
