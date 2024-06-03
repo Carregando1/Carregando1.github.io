72840c16fa525b510cf54c24524bff223f6c12c2
Carregando1 <darrenys2015@gmail.com>
2024-06-02 23:55:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var nresources = [];
 var sc = 0;
 var as = 0;
 let screencheck;
+let gameintv;
 function onloadp() {
 	if (window.innerWidth < 820) {
    		document.getElementById("totaldiv").innerHTML="<p>To play Idle Prestiges, you need a window of width 820px or wider. Resize your window, swap orientation, or swap devices to continue.</p><p>Current width: "+window.innerWidth+"px";
@@ -14,5 +15,15 @@ function widthcheck() {
 	if (window.innerWidth >= 820) {
 		document.getElementById('totaldiv').innerHTML="<div class='mainbar' id='mainbar1'><button onclick='pg(1)' class='topbar'>Prestige Layers</button></div><div class='mainbar' id='mainbar2'><button onclick='pg(2)' class='topbar'>Locked</button></div><div class='mainbar' id='mainbar3'><button onclick='pg(3)' class='topbar'>Achievements</button></div><div class='mainbar' id='mainbar4'><button onclick='pg(4)' class='topbar'>Statistics</button></div><div id='maincontent'></div>";
 		clearInterval(screencheck);
+	} else {
+		document.getElementById("totaldiv").innerHTML="<p>To play Idle Prestiges, you need a window of width 820px or wider. Resize your window, swap orientation, or swap devices to continue.</p><p>Current width: "+window.innerWidth+"px";
 	}
+}
+function init() {
+	//Starting code init
+	screencheck = setInterval(widthcheck, 20);
+	gameintv = setInterval(update, 1000);
+}
+function update() {
+	//function code
 }
