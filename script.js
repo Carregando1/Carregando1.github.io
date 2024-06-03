3b003ae80b1503596b5669551f0587172f2c86b9
Carregando1 <darrenys2015@gmail.com>
2024-06-02 22:31:04 -0600

    E1.6

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function onloadp() {
 }
 function widthcheck() {
 	if (window.innerWidth >= 820) {
-		document.getElementById('totaldiv').innerHTML='<div class='mainbar' id='mainbar1'><button onclick='pg(1)' class='topbar'>Prestige Layers</button></div><div class='mainbar' id='mainbar2'><button onclick='pg(2)' class='topbar'>Locked</button></div><div class='mainbar' id='mainbar3'><button onclick='pg(3)' class='topbar'>Achievements</button></div><div class='mainbar' id='mainbar4'><button onclick='pg(4)' class='topbar'>Statistics</button></div><div id='maincontent'></div>";
+		document.getElementById('totaldiv').innerHTML="<div class='mainbar' id='mainbar1'><button onclick='pg(1)' class='topbar'>Prestige Layers</button></div><div class='mainbar' id='mainbar2'><button onclick='pg(2)' class='topbar'>Locked</button></div><div class='mainbar' id='mainbar3'><button onclick='pg(3)' class='topbar'>Achievements</button></div><div class='mainbar' id='mainbar4'><button onclick='pg(4)' class='topbar'>Statistics</button></div><div id='maincontent'></div>";
 		clearInterval(screencheck);
 	}
 }
